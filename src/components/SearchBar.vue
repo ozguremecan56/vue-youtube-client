69bd8ae172b13f214ea8ca764379ed473ac1c385
ozguremecan56 <ozguremecan56@gmail.com>
2021-03-02 09:27:32 +0300

    first commit

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <div>
+     <input v-on:input="onInput"/>
+    </div>
+
+</template>
+
+<script>
+
+
+
+export default {
+    name:'SearchBar',
+    methods: {
+        onInput:(event)=>{
+            this.$emit('termChange', event.target.value) //dollar sign isnt special. vue makers just wanted to call that function $emit
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
+import Vue from 'vue';
+import App from './App'
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    //el:'#app', //mount vue to app id element
+    render:h => h(App) //h stands for createElement function. pass h to your function,than pass App to it in your func
+}).$mount('#app') //another way of mounting vue
